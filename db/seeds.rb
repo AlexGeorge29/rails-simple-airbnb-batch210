f35c53a1985c1cbc4507ee9cb1c8725ad5f148ce
alex george <alexandreantoinegeorge@gmail.com>
2019-02-12 19:28:03 -0500

    model generation - migration 1 - seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning Databas"
+  Flat.destroy_all
+
+puts "generating database"
+
+4.times do
+Flat.create!(
+  name: Faker::FunnyName.name,
+  address: Faker::Address.full_address,
+  description: Faker::Lorem.paragraph,
+  price_per_night: (1..10).to_a.sample * 100,
+  number_of_guest: (1..10).to_a.sample
+)
+end
