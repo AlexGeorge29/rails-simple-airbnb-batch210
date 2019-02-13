aa9e78aba04de501bad6d88bc802363a40365eaf
alex george <alexandreantoinegeorge@gmail.com>
2019-02-12 20:04:59 -0500

    edit and update features added

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,6 +1,7 @@
 class FlatsController < ApplicationController
   def index
     @flats = Flat.all
+    new
   end
 
   def show
@@ -14,12 +15,21 @@ class FlatsController < ApplicationController
   def create
     @flat = Flat.new(flat_params)
     if @flat.save
-      root_to flat_path(@flat)
+      redirect_to flat_path(@flat)
     else
       render :new
     end
   end
 
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    edit
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
   private
 
   def flat_params
