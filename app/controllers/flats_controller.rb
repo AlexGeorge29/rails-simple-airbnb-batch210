dcc08b82a8e61d7e78c17a61b4c59674e9a086a0
alex george <alexandreantoinegeorge@gmail.com>
2019-02-12 19:45:36 -0500

    show and index done with view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,25 +1,28 @@
 class FlatsController < ApplicationController
-def index
-  @flats = Flat.all
-end
+  def index
+    @flats = Flat.all
+  end
 
-def show
+  def show
+    @flat = Flat.find(params[:id])
+  end
 
-end
+  def new
+    @flat = Flat.new
+  end
 
-def new
-  @flat = Flat.new
-end
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      root_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
 
-def create
-  @flat = Flat.new(flat_params)
-  if @flat.save
-    root_to flat_path(@flat)
-  else
-    render :new
-end
+  private
 
-def flat_params
-
-end
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guest)
+  end
 end
