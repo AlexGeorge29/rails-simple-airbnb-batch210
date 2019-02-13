4131e04c72ee63cb3514fcb7c540e840d4720648
alex george <alexandreantoinegeorge@gmail.com>
2019-02-12 19:38:43 -0500

    add some features

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,25 @@
 class FlatsController < ApplicationController
+def index
+  @flats = Flat.all
+end
+
+def show
+
+end
+
+def new
+  @flat = Flat.new
+end
+
+def create
+  @flat = Flat.new(flat_params)
+  if @flat.save
+    root_to flat_path(@flat)
+  else
+    render :new
+end
+
+def flat_params
+
+end
 end
